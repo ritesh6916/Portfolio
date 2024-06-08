f3c443f6118e80040d72a450261c6fcb4937ce65
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-06-08 15:29:23 +0530

    Profile Pic Added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/akkPP.png";
+import profilePic from "../assets/ProfileImg.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
@@ -43,7 +43,7 @@ function Hero() {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <motion.img initial={{x:100, opacity: 0}} animate={{x:0, opacity:1}} transition={{duration: 1, delay: 1.2}} src={profilePic} alt="Aditya" />
+            <motion.img initial={{x:100, opacity: 0}} animate={{x:0, opacity:1}} transition={{duration: 1, delay: 1.2}} src={profilePic} alt="Ritesh" />
           </div>
         </div>
       </div>
